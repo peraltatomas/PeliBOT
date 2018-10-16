83a84a6f18384aa2b081e6aea18dac582e312d8a
Tomas Peralta <contacto@tomasperalta.com.ar>
2018-10-16 12:05:48 -0300

    First commit

---
+++ config.js
@@ -0,0 +1,7 @@
+// Add your Twitter API Credentials
+module.exports = {
+    consumer_key:         'bXHYtSW5GNKbxMbxwLF0O9i5A',
+    consumer_secret:      '6R5DOVwfdiypJrnbp1qAe7elaXPXqRC9Zk7i0dLYah2hu6tb5m',
+    access_token:         '1051859656086294529-uIoBymybApW6BZlv0EeFkIPVxW3GqB',
+    access_token_secret:  'e4gb4yejSRUs7Hk3aDH0NkIIoo4dKEj9gMDWR7EfO8FUX'
+}
