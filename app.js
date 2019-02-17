233a100bf280e750897839851d232f35a8c53301
Tomas Peralta <contacto@tomasperalta.com.ar>
2019-02-17 14:34:11 -0300

    Makes TwitBOT reply the mention

--- app.js
+++ app.js
@@ -16,18 +16,18 @@ function checkEvent(eventMsg) {
     var replyTo = eventMsg.in_reply_to_screen_name;
 
     if (replyTo === 'nosequever') {
-        getRandomMovie(eventMsg.user.screen_name);
+        getRandomMovie(eventMsg.user.screen_name, eventMsg.id_str);
     }
 }
 
 // Searches for a random movie within the Open Movie DataBase API
-// The movie should have an Average Score of 7 points and at least 20 reviews
+// The movie should have an Average Score of 7 points and at least 50 reviews
 // So the bot doesn't recommend shitty movies.
-function getRandomMovie(user) {
+function getRandomMovie(user, mentionID) {
     // TheMovieDB credentials
     var api_key = require('./config.js').movies_database_api_key;
     var api_page = Math.floor((Math.random() * 10) + 1); // Random results page
-    var api_url = 'https://api.themoviedb.org/3/discover/movie?api_key=' + api_key + '&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=' + api_page + '&vote_count.gte=20&vote_average.gte=7';
+    var api_url = 'https://api.themoviedb.org/3/discover/movie?api_key=' + api_key + '&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=' + api_page + '&vote_count.gte=50&vote_average.gte=7';
 
     // API Request
     var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
@@ -44,18 +44,23 @@ function getRandomMovie(user) {
             var moviePosterURL = 'https://image.tmdb.org/t/p/w500' + movie['poster_path '];
             var movieReleaseYear = movie['release_date'].split("-")[0];
 
-            toTweet = "@" + user + " hoy podrías ver " + movieTitle + " (" + movieReleaseYear + "). Más info: " + movieURL;
+            toTweet = "@" + user + " Hoy podrías ver " + movieTitle + " (" + movieReleaseYear + "). Más info: " + movieURL;
         } else {
-            toTweet = "@" + user + " algo salió mal al intentar recomendarte una película. Puedes intentarlo de nuevo más tarde?";
+            toTweet = "@" + user + " Algo salió mal al intentar recomendarte una película. Puedes intentarlo de nuevo más tarde?";
         }
-        tweetIt(toTweet);
+        tweetIt(toTweet, mentionID);
     }
     request.send();
 }
 
 // Posts a tweet on Twitter
-function tweetIt(tweet) {
-    peliBot.post('statuses/update', { status: tweet }, tweeted);
+function tweetIt(tweet, mentionID) {
+    var params = {
+        status: tweet,
+        in_reply_to_status_id: mentionID
+    }
+
+    peliBot.post('statuses/update', params, tweeted);
 
     function tweeted(err, data, response) {
         if (err) {
